79e794295f3b2db0669d87c6a25f8996d2048bff
Deepak Sood <deepak15013@iiitd.ac.in>
2017-06-16 09:25:28 +0530

    UI update

--- app/src/main/java/io/github/varunj/sangoshthi_ivr/activities/LoginActivity.java
+++ app/src/main/java/io/github/varunj/sangoshthi_ivr/activities/LoginActivity.java
@@ -43,8 +43,8 @@ public class LoginActivity extends AppCompatActivity {
         AMQPPublish.getInstance().publishToAMQP();
 
         SharedPreferenceManager.getInstance().init(getApplicationContext());
-        //if(!SharedPreferenceManager.getInstance().getSession()) {
-        if(true) {
+        if(!SharedPreferenceManager.getInstance().getSession()) {
+        //if(true) {
             // first time login, get phone number
             final EditText etPhone = (EditText) findViewById(R.id.et_phone);
             Button btnSignIn = (Button) findViewById(R.id.btn_sign_in);
